d979e28becfb0c9c136809d91a2190409bc142c9
alfadev1 <alfarollano@gmail.com>
2023-12-01 01:23:12 -0300

    primer commit

---
+++ todoapp/src/test/java/com/alfadev1APIREST/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfadev1APIREST.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
