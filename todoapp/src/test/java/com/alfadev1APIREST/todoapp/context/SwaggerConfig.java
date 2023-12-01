cb54596ff3322be847db1125a2c7216dd4869c15
alfadev1 <alfarollano@gmail.com>
2023-12-01 03:28:28 -0300

    clase SwaggerConfig agregada

---
+++ todoapp/src/test/java/com/alfadev1APIREST/todoapp/context/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.alfadev1APIREST.todoapp.context;
+
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author alfadev1
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.alfadev1APIREST.todoapp.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "ToDoApi",
+                "Practica de APIrest",
+                "v1",
+                "termsOfService",
+                new Contact("alfadev1", "https://github.com/alfadev1", "alfarollano@gmail.com"), 
+                "Licencia de API",
+                "ejempliLicenciaUrl", 
+                Collections.emptyList());
+    }
+}
