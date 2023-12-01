c5a09e5ec4857e13d96a3cc1d9475967d8b2390f
alfadev1 <alfarollano@gmail.com>
2023-12-01 04:45:11 -0300

    clase TaskController paquete controller

---
+++ todoapp/src/test/java/com/alfadev1APIREST/todoapp/controller/TaskController.java
@@ -0,0 +1,14 @@
+
+package com.alfadev1APIREST.todoapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author cyka
+ */
+@RestController
+public class TaskController {
+    
+    
+}
